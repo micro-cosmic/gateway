4742404253528561244efdffccb19ef819de2665
caojiancheng <jiancheng.cao@yi23.net>
2021-08-25 12:41:19 +0800

    首次提交

---
+++ src/main/java/com/micro/cosmic/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.micro.cosmic.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caojiancheng
+ * @date 2021/8/24
+ * @description
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
